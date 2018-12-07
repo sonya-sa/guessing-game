5396b38a4fe2b6c850210dc5203f65b73fea31e9
vagrant <vagrant@vagrant.vm>
2018-12-07 20:47:46 +0000

    flask app connects to server and renders demo html template

---
+++ server.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+#create an instance of the flask app.
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    #set to True for debugging
+    #set to False for production
+    app.debug = True
+    
+    app.run(host="0.0.0.0")
