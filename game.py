028318164470349fd20744814df83cde380d8a93
vagrant <vagrant@vagrant.vm>
2018-12-09 01:15:35 +0000

    completed terminal based game to be used as a model; game limits attempts, shows spaces, allows word submission or letter submission

--- game.py
+++ game.py
@@ -1,51 +1,154 @@
-# #create a word guessing game where the user play the "computer"
-# #user has a maximum of 6 tries to guess correctly
+# # #create a word guessing game where the user play the "computer"
+# # #user has a maximum of 6 tries to guess correctly
 
 # import random
-# #import words from word_dict
-
-# words = ['hello', 'bye']
-
-# class Game:
-#     """Game rules and logic."""
-
-#     def __init__(self, words):
-#         self.word = random.choice(word_list)
-#         self.max_incorrect_guesses = 6
-
-#     #function gets word
-    #function displays spaces
-    #function checks letter
-    #function checks word
-    #function checks guesses
-    #function counts guesses
-    #function counts remaining guesses
-    #fuction checks winner
-    #function checks loser
 
 
-    # def display_spaces(self):
 
-    #     for letter in range(len(self.word)):
-    #         display[letter] = '_'
 
+# wordlist = ["hello", "bye"]
+
+# word = random.choice(wordlist).lower()
+
+# display = []
+
+# used = []
+
+# display.extend(word)
+
+# used.extend(display)
+
+# for i in range(len(display)):
+#     display[i] = '_'
+
+# print (' '.join(display))
+# print ()
+
+
+# def main():
+
+#     counter = 0
+
+#     incorrect = 6
+
+#     while counter < len(word) and incorrect > 0 :
+
+#         guess = input("Please enter a letter: ")
+#         guess = guess.lower()
+#         print("Number of incorrect guesses remaining: {} ".format\
+#             (incorrect))
+
+#         # if guess not in used:
+#         #     used.append(guess)
+#         # else:
+#         #     print ("You already guessed that letter. Try again!")
+
+#         for i in range(len(word)):
+
+#             if word[i] == guess and guess not in used:
+
+#                 display[i] = guess
+
+#                 counter = counter + 1
+
+#                 used.remove(guess)
+
+#         if guess not in display:
+
+#             incorrect = incorrect - 1
+#             print ("Sorry, that's incorrect. You have", incorrect, \
+#                 'chances remaining.')
+
+
+#     if counter == len(word):
+#         print ("Congratulations! You found the word.")
+
+#     else:
+#         print ("Game over.")
+
+# main()
 
 import random
+#create a function that randomly selects a word
 
-#create a list of possible words for the game
-wordlist = ["frazzled", "grogginess", "crypt", "ostracize", "oxygen", \
-            "rhythmic", "pajama", "jinx", "yacht", "banjo", "awkward", \
-            "zigzag", "twelfth", "unzip", "mystify", "jukebox"]
+def get_word():
+    wordlist = ['hello']
+    return random.choice(wordlist).lower()
 
-random.choice(wordlist).lower()
+#create function that checks word and guess
+#function has 3 paramaters: word selected, guess letter, list of guesses taken
+def check(word, guesses, guess):
+    show = ''
+    matches = 0
 
-word = list(wordlist[0])
+    for letter in word:
+        if letter == guess:
+            matches += 1
+        if letter in guesses:
+            show += letter
+        else:
+            show += '-'
+    if matches > 1:
+        print('Yes, there are {} {}'.format(matches,guess) + "'"+ 's in the word.')
+    elif matches == 1:
+        print ('Yes! The word contains the letter: ' + guess + "'")
+    else:   
+        print (show)
+        print ('Sorry. The word does not contain the letter "' + guess + '".')
 
-display = []
+    return show
 
-used = []
+#main function that runs game
+def main():
+    #call get_word function and assign to word variable
+    word = get_word()
+    #print(word)
+    guesses = []
+    guessed = False
+    incorrect = 0
+    max_guesses = 6
 
-display.extend(word)
+    while not guessed and incorrect < max_guesses:
+        text = 'Please enter one letter or a {}-letter word: '.format(len(word))
+        print('You have already guessed: {}'.format(guesses))
+        guess = input(text).lower()
+        
+        #if we already guessed the letter, try again
+        if guess in guesses:
+            print('You already guessed:' + guess + ". Try again.")
+        
+        #check word for match
+        elif len(guess) == len(word):
+            guesses.append(guess)
+            if guess == word:
+                guessed = True
+            
+            #word was incorrect guess
+            else:
+                incorrect += 1
+                print ('Sorry! That is incorrect.')
+        
+        elif len(guess) == 1:
+            guesses.append(guess)
 
+            #returns a string of checked guesses
+            result = check(word,guesses,guess)
+            
+            #if guess not in return string, then letter was incorrect 
+            if guess not in result:
+                incorrect += 1
+            elif result == word:
+                guessed = True
+            else:
+                print(result)
+        else:
+            print('Invalid entry.')
+    
+    if incorrect == max_guesses:
+        print ('Sorry! You hit the maximum trys. You lose.')
+    
+    else:
+        print ('Yes, the word is', word + '. It took you', len(guesses), 'tries.')
 
-#used.extend(display)
+main()
+
