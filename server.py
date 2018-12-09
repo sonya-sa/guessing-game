ec5df79e851aa718ec5baba47c1e142228bd65cd
vagrant <vagrant@vagrant.vm>
2018-12-09 18:57:35 +0000

    added logic to select word by difficulty

--- server.py
+++ server.py
@@ -1,12 +1,42 @@
-from flask import Flask, render_template
+from jinja2 import StrictUndefined
+from flask import Flask, render_template, request, flash, redirect, session
+#from flask_debugtoolbar import DebugToolbarExtension
+
+from word_dict import get_word
+import requests
+import random
 
 #create an instance of the flask app.
 app = Flask(__name__)
 
+# Required to use Flask sessions and the debug toolbar
+app.secret_key = "ABC"
+
+# Normally, if you use an undefined variable in Jinja2, it fails silently.
+# This is horrible. Fix this so that, instead, it raises an error.
+app.jinja_env.undefined = StrictUndefined
+
+
 @app.route('/')
 def index():
     """Homepage."""
+
     return render_template('index.html')
+
+@app.route("/select-difficulty")
+def word_by_difficulty():
+    """Retrieves difficulty level when user selects a number between 1-10."""
+
+    difficulty = request.args.get("difficulty")
+    return difficulty
+
+@app.route("/secret-word")
+def get_secret_word(difficulty):
+
+    secret_word = get_word(difficulty)
+    return render_template(secret_word=secret_word)
+
+  
 
 if __name__ == "__main__":
     #set to True for debugging
